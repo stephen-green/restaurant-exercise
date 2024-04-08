b2d8c13cf685ecea511a60ba4ef3d37ff27cca67
stephen-green <50608901+stephen-green@users.noreply.github.com>
2024-04-08 10:01:15 -0700

    Create basic UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <>
+      <section>
+        <h1>Restaurant File Import</h1>
+        <input id="restaurant-file-selector" type="file" accept=".csv" />
+      </section>
+      
+      <section>
+        <h1>Best Restaurants</h1>
+        <ul></ul>
+      </section>
+    </>
   );
 }
 

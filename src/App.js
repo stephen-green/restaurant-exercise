58e2c1efbf165174a68160168e52d38c7f9108fd
stephen-green <50608901+stephen-green@users.noreply.github.com>
2024-04-08 10:47:40 -0700

    Support line-by-line import
    Skip header, still need to parse CSV

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
+  
+  function handleRestaurantFileSelected(e) {
+    let reader = new FileReader();
+    reader.addEventListener('load', e2 => setRestaurants(reader.result.split(/\r\n|\n|\r/g).slice(1))); // Skip first line, assumed to be header.
+    reader.readAsText(e.target.files[0]);
+  }
+  
   return (
     <>
       <section>
         <h1>Restaurant File Import</h1>
-        <input id="restaurant-file-selector" type="file" accept=".csv" />
+        <input id="restaurant-file-selector" type="file" accept=".csv" onChange={handleRestaurantFileSelected} />
       </section>
       
       <section>
         <h1>Best Restaurants</h1>
-        <ul></ul>
+        <ul>{restaurants.map(restaurant => <li>{restaurant}</li>)}</ul>
       </section>
     </>
   );

c5ed1874f0efadbd1203e20ef029c8a4d4b45753
stephen-green <50608901+stephen-green@users.noreply.github.com>
2024-04-21 19:57:55 -0700

    Got rid of old project files, retained new project files (Vite) in root directory

--- old-create-react-app/src/classes/MetricNormalizer.js
+++
@@ -1,6 +0,0 @@
-export interface MetricNormalizer {
-  /*
-   * Applies this metric to a range of values, normalizing them on a scale from 0 (bad) to 1 (good)
-   */
-  normalize(range: number[]): number[];
-}
